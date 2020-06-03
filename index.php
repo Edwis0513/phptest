dcbc91b2cb93299a7200c223525fd7fb5f5a871d
Edwis0513 <ed.jonusas@gmail.com>
2020-06-03 13:07:22 +0300

    12 done

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
+
 <?php
-//3. Priskirkite dviems konstantoms kokias nors reikšmes ir jas sujunkite. Atspausdinkite.
-define('PIRMA', 'Dainius');
-define('ANTRA', 'Giedrius');
-print PIRMA . ' ' . ANTRA;
+//12. Turime masyvą:
+//Padidinkite masyvo elementų reikšmes 10 kartų ir atspausdinkite foreach pagalba. Tekstinės lieka nepakeistos.
+$a = array (10,20,30, 1, 1 ,50, 0, 0, "jonas", "petras");
+
+$filtered_num = array_filter($a, 'is_int');
+$filtered_string = array_filter($a, 'is_string');
+$filtered_num[] = $value * 60;
+
+$a = array_merge($filtered_num, $filtered_string);
+
+foreach ($a as $value) {
+    print($value);
+   }
