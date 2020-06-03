775485680b7c410abbe21498cb01629cddfc1024
Edwis0513 <ed.jonusas@gmail.com>
2020-06-03 10:46:49 +0300

    10 done

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 
 <?php
-// 9. Turime kintamuosius ir reikšmes $vardas, $pavardė ir $atlyginimas. 
-// Parašykite sąlygą, jeigu egzistuoja kinamieji $vardas ir $pavarde, o $atlyginimas 
-//> 5000, spausdinti - visi duomenys yra; kitu atveju spausdinti: duomenys netinkami.
+//10. Ką daryti, kad reikšmes naršyklėje rodytų skirtingose eilutėse:
 
-$vardas = "";
-$pavarde = "";
-$atlyginimas = "6000";
 
-if (empty($vardas) || empty($pavarde) || $atlyginimas < 5000) {
-    print "duomenys netinkami";
-} else {
-    print "visi duomenys yra";
-}
+echo "a \n <br> b \n <br> c \n <br> d";
+
+
 ?>
